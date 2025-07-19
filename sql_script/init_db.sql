d6e6e456b9b9a82ab954fc8fc5b9078731168d1b
victorr1809 <haducmanh990@gmail.com>
2025-07-19 11:17:09 +0700

    Create init_db.sql

---
+++ sql_script/init_db.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE "Data warehouse"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+	
+CREATE SCHEMA IF NOT EXISTS bronze
+    AUTHORIZATION postgres;
+CREATE SCHEMA IF NOT EXISTS silver
+    AUTHORIZATION postgres;
+CREATE SCHEMA IF NOT EXISTS gold
+    AUTHORIZATION postgres;
+
