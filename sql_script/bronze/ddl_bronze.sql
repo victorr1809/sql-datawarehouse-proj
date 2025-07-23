4c71bfc93fc1630161263ce349e4142b7222fa7b
victorr1809 <haducmanh990@gmail.com>
2025-07-23 17:07:59 +0700

    Update ddl_bronze.sql

--- sql_script/bronze/ddl_bronze.sql
+++ sql_script/bronze/ddl_bronze.sql
@@ -1,1 +1,15 @@
+-- Tạo bảng và cột cho lớp bronze:
+
+CREATE TABLE bronze.crm_sales_details (
+	sls_ord_num varchar(50),
+	sls_prd_key varchar(50),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT
+);
+
 
